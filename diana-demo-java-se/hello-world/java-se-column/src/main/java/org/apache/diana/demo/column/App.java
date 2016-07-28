8acc005f11f506256eb4d21ccb60bcff02b94848
Otavio <otaviojava@java.net>
2016-07-27 21:23:04 -0300

    Add column example

---
+++ diana-demo-java-se/hello-world/java-se-column/src/main/java/org/apache/diana/demo/column/App.java
@@ -0,0 +1,39 @@
+package org.apache.diana.demo.column;
+
+
+import org.apache.diana.api.column.*;
+import org.apache.diana.cassandra.column.CassandraConfiguration;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class App {
+
+    public static final String KEY_SPACE = "newKeySpace";
+    public static final String COLUMN_FAMILY = "newColumnFamily";
+
+    public static void main(String[] args) {
+
+        ColumnConfiguration condition = new CassandraConfiguration();
+
+        try(ColumnFamilyManagerFactory managerFactory = condition.getManagerFactory()) {
+            ColumnFamilyManager columnEntityManager = managerFactory.getColumnEntityManager(KEY_SPACE);
+            ColumnFamilyEntity entity = ColumnFamilyEntity.of(COLUMN_FAMILY);
+            Column id = Column.of("id", 10L);
+            entity.add(id);
+            entity.add(Column.of("version", 0.001));
+            entity.add(Column.of("name", "Diana"));
+            entity.add(Column.of("options", Arrays.asList(1, 2, 3)));
+
+            columnEntityManager.save(entity);
+
+            ColumnQuery query = ColumnQuery.of(COLUMN_FAMILY);
+            query.addCondition(ColumnCondition.eq(id));
+            List<ColumnFamilyEntity> columnFamilyEntities = columnEntityManager.find(query);
+            System.out.println(columnFamilyEntities);
+
+        }
+
+
+    }
+}
