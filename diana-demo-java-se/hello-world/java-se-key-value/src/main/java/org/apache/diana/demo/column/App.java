97d111f7135fc1b9d37a652966331a10f906769b
Otavio <otaviojava@java.net>
2016-07-29 00:26:11 -0300

    Adds key-value demo

---
+++ diana-demo-java-se/hello-world/java-se-key-value/src/main/java/org/apache/diana/demo/column/App.java
@@ -0,0 +1,31 @@
+package org.apache.diana.demo.column;
+
+
+import org.apache.diana.api.Value;
+import org.apache.diana.api.key.BucketManager;
+import org.apache.diana.api.key.BucketManagerFactory;
+import org.apache.diana.api.key.KeyValueConfiguration;
+import org.apache.diana.hazelcast.key.HazelCastKeyValueConfiguration;
+
+import java.util.*;
+
+public class App {
+
+
+    public static void main(String[] args) {
+
+        KeyValueConfiguration configuration = new HazelCastKeyValueConfiguration();
+        try (BucketManagerFactory managerFactory = configuration.getManagerFactory()) {
+            BucketManager bucket = managerFactory.getBucketManager("bucket");
+            List<String> list = managerFactory.getList("bucketList", String.class);
+            Set<String> set = managerFactory.getSet("bucketSet", String.class);
+            Map<String, Integer> map = managerFactory.getMap("bucketList", String.class, Integer.class);
+            Queue<String> queue = managerFactory.getQueue("queueList", String.class);
+            bucket.put("key", "value");
+            Optional<Value> value = bucket.get("key");
+        }
+
+
+    }
+}
+
