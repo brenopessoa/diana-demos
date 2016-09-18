32452c43f265d6e18d86fab6f5c16ac6a83c575c
Otavio Santana <otaviojava@java.net>
2016-09-18 07:27:21 -0700

    Fixes document se example

--- diana-se/document/src/main/java/org/jnosql/diana/demo/App.java
+++ diana-se/document/src/main/java/org/jnosql/diana/demo/App.java
@@ -17,16 +17,16 @@ public class App {
         try(DocumentCollectionManagerFactory collectionFactory = configuration.getManagerFactory();) {
             DocumentCollectionManager collectionManager = collectionFactory.getDocumentEntityManager(DATABASE);
 
-            DocumentCollectionEntity entity = DocumentCollectionEntity.of(DOCUMENT_COLLECTION);
+            DocumentEntity entity = DocumentEntity.of(DOCUMENT_COLLECTION);
             entity.add(Document.of("name", "Daniel Soro"));
             entity.add(Document.of("age", 26));
 
-            DocumentCollectionEntity entitySaved = collectionManager.save(entity);
+            DocumentEntity entitySaved = collectionManager.save(entity);
             Optional<Document> id = entitySaved.find("_id");
 
             DocumentQuery query = DocumentQuery.of(DOCUMENT_COLLECTION);
             query.addCondition(DocumentCondition.eq(id.get()));
-            List<DocumentCollectionEntity> documentsFound = collectionManager.find(query);
+            List<DocumentEntity> documentsFound = collectionManager.find(query);
 
 
         }
